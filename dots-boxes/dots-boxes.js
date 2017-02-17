60c0031aba78666d25c4065fdb2eb60c76e1eb90
Keith Collins <keithcollins01@gmail.com>
2017-02-16 22:09:49 -0500

    Update dots-boxes.js

--- dots-boxes/dots-boxes.js
+++ dots-boxes/dots-boxes.js
@@ -1,9 +1,21 @@
 (function(){
+  class Dot {
+    constructor(id,x,y) {
+      this.id = id;
+      this.x = x;
+      this.y = y;
+    }
+  }
+
+  function heuristic(dotA, dotB) {
+    return Math.abs(dotA.x - dotB.x) + Math.abs(dotA.y - dotB.y);
+  }
   // Define constants
   // Some will be user-defined
   var NUM_BOXES = 9;
   var BOXES_PER_SIDE = 3;
   var DOT_RADIUS = 40;
+  var NUM_LINES = 12;
   var WIDTH = 405;
   var HEIGHT = 720;
   var INNDER_WIDTH = WIDTH - (DOT_RADIUS*2);
@@ -16,6 +28,7 @@
   // which are equal to DOT_RADIUS
   var svg = d3.select("#game").append("svg")
     .attr("viewBox", "0 0 "+WIDTH+" "+HEIGHT)
+    .on("mousemove",mousemove)
     .append("g").attr("transform","translate("+DOT_RADIUS+","+DOT_RADIUS+")");
 
   // Create an array of objects with
@@ -23,27 +36,43 @@
   var yIndex = -1;
   var dotGrid = d3.range(NUM_BOXES).map(function(d,i){
     yIndex += (i%BOXES_PER_SIDE == 0) ? 1 : 0;
-    return {
-      xPos: DOT_SCALE(i%BOXES_PER_SIDE),
-      yPos: DOT_SCALE(yIndex),
-      id: i
-    };
+    return new Dot(i,DOT_SCALE(i%BOXES_PER_SIDE),DOT_SCALE(yIndex));
   });
+
+  var linesG = svg.append("g");
+  var moveline = null;
 
   var dots = svg.selectAll(".dot")
     .data(dotGrid)
     .enter().append("circle")
     .attr("class","dot")
     .attr("id",function(d){ return "d"+d.id})
-    .attr("cx",function(d){ return d.xPos})
-    .attr("cy",function(d){ return d.yPos})
+    .attr("cx",function(d){ return d.x})
+    .attr("cy",function(d){ return d.y})
     .attr("r",DOT_RADIUS);
 
   dots.on("mousedown",function(){
-    var startDot = d3.select(this)
+    var startDotEl = d3.select(this)
       .style("stroke","#69D2E7")
       .style("stroke-width",5);
 
+    var startDot = startDotEl["0"]["0"].__data__;
+
+    moveline = linesG.append("line")
+      .attr("class","line moveline")
+      .attr("x1",startDotEl.attr("cx"))
+      .attr("y1",startDotEl.attr("cy"))
+      .attr("x2",startDotEl.attr("cx"))
+      .attr("y2",startDotEl.attr("cy"));
   });
+
+  function mousemove() {
+    if (moveline) {
+      var coords = d3.mouse(this);
+      moveline
+        .attr("x2",coords[0]-DOT_RADIUS)
+        .attr("y2",coords[1]-DOT_RADIUS);
+    }
+  }
 
 })()
